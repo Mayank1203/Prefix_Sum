79d41e53830ba2c9c9729f25ccb3bfc5ff224d1b
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-08 16:33:58 +0530

    finding Pivot Element with Prefix sum

---
+++ src/PivotIndex.java
@@ -0,0 +1,29 @@
+public class PivotIndex {
+    public static void main(String[] args) {
+
+        int[] nums = {1,7,3,6,5,6};
+        System.out.println(pivotIndex(nums));
+    }
+
+    public static int pivotIndex(int[] nums) {
+        int totalSum = 0;
+        for (int num : nums) {
+            totalSum += num;
+        }
+
+        int leftSum= 0 ;
+        for(int i =0 ; i<nums.length; i++){
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum){
+                return i ;
+            }
+            leftSum += nums[i] ;
+
+        }
+
+        return -1 ;
+    }
+}
+
+// Question Link -> https://leetcode.com/problems/find-pivot-index/
