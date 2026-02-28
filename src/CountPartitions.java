ef64e4113f34d809449a16d390c6b1c75e89a374
Mayank1203 <mayankpatle1203@gmail.com>
2026-02-28 11:46:16 +0530

    Prefix sum used to obtain partitions

---
+++ src/CountPartitions.java
@@ -0,0 +1,31 @@
+public class CountPartitions {
+    public static void main(String[] args) {
+        int[] nums = {10,10,3,7,6};
+        System.out.println(countPartition(nums));
+
+    }
+
+    public static int countPartition(int[] nums) {
+        int totalSum = 0;
+        for (int i=0;i<nums.length;i++) {
+            totalSum += nums[i];
+        }
+
+        int leftSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            leftSum += nums[i];
+            int rightSum = totalSum - leftSum;
+
+            if ((leftSum % 2) == (rightSum % 2)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+
+// Question Link -> https://leetcode.com/problems/count-partitions-with-even-sum-difference/
